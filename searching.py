a678aee7f3b7d2e2461cf849d04eecb0bf9133d3
Zien Alhaoushe <zienalhaoushe@hotmail.com>
2020-08-28 01:33:33 +0300

    Update searching.py

--- searching.py
+++ searching.py
@@ -5,6 +5,7 @@ scraping the definition
 """
 
 
+
 import json
 from difflib import get_close_matches
 
@@ -12,30 +13,34 @@ data = json.load(open("076 data.json"))
 
 
 def translate(word):
-    word = word.lower()
     if word in data:
-
         return data[word]
-    elif len(get_close_matches(word, data.keys())) > 0:
-        yn = input("Did you mean {} instead? enter Y if yes and N if no".format(get_close_matches(word, data.keys())[0]))
-        if yn == "Y":
-            return data[get_close_matches(word, data.keys())[0]]
-        elif yn == "N":
-            return "Please double check your "
-        else:
-            return "Sorry we don't uderstand your query "
-
-
     else:
-        return "This word is not available now in our dectionry help us and at it from here!"
+        word=word.lower()
+        if word in data:
+            return data["word"]
+        elif len(get_close_matches(word, data.keys())) > 0:
+            yn = input("Did you mean {} instead? enter Y if yes and N if no".format(get_close_matches(word, data.keys())[0]))
+            if yn == "Y":
+                return data[get_close_matches(word, data.keys())[0]]
+            elif yn == "N":
+                return "Please double check your "
+            else:
+                return "Sorry we don't uderstand your query "
 
 
-w = input("Enter your word please ")
-output = translate(w)
-if  type(output) == list:
+        else:
+            return "This word is not available now in our dectionry help us and at it from here!"
 
-    for sentence in  output:
+
+word = input("Enter word: ")
+output = translate(word)
+if type(output) == list:
+
+    for sentence in output:
         print(sentence)
 else:
     print(output)
+
+
 
